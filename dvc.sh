26d95423432b162bbd94a10b2f3a4c3a4d4b07a5
Jesús Aransay <jmaransay@gmail.com>
2025-05-07 00:08:44 +0200

    Update dvc.sh

--- dvc.sh
+++ dvc.sh
@@ -4,12 +4,18 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Actualizamos repositorios:
 apt-get update
+apt-get install unzip tree
+apt-get install python3-pip
+apt-get install python3.12-venv
+export PATH=$PATH:/home/vagrant/.local/bin
 
-apt-get install -y unzip tree
-
-apt-get install -y python3-pip
-
-apt-get install -y python3.10-venv
+apt-get install wget gpg
+mkdir -p /etc/apt/keyrings
+wget -qO - https://dvc.org/deb/iterative.asc | sudo gpg --dearmor -o /etc/apt/keyrings/packages.iterative.gpg
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.iterative.gpg] https://dvc.org/deb/ stable main" | sudo tee /etc/apt/sources.list.d/dvc.list
+chmod 644 /etc/apt/keyrings/packages.iterative.gpg /etc/apt/sources.list.d/dvc.list
+apt-get update
+apt-get install dvc
 
 # locate pip3
 
